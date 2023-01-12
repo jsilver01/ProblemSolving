54512edc1238ccb64e08cfac69501db1de76cb4b
JUNGEUN <122519994+jsilver01@users.noreply.github.com>
2023-01-12 21:45:22 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+    int score;
+    scanf("%d",&score);
+    if(score>=90){
+        printf("A");
+    }
+    else if(score<90&&score>=80){
+        printf("B");
+    }
+    else if(score<80&&score>=70){
+        printf("C");
+    }
+    else if(score<70&&score>=60){
+        printf("D");
+    }
+    else{
+        printf("F");
+    }
+    return 0;
+}
