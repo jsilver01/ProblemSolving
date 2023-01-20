eb06770a666f6893fbe1be7907e17d9f40f4464f
JUNGEUN <122519994+jsilver01@users.noreply.github.com>
2023-01-20 23:32:25 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main() {
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	
+	if (a == b && b == c) {
+		printf("%d", 10000 + a * 1000);
+	}
+	else if (a == b && b != c) {
+		printf("%d", 1000 + a * 100);
+	}
+	else if (a == c && a != b) {
+		printf("%d", 1000 + a * 100);
+	}
+	else if (b == c && a != b) {
+		printf("%d", 1000 + b * 100);
+	}
+	else {
+		if (a > b) {
+			if (c > a) {
+				printf("%d", c * 100);
+			}
+			else {
+				printf("%d", a * 100);
+			}
+		}
+		else {
+			if (c > b) {
+				printf("%d", c * 100);
+			}
+			else {
+				printf("%d", b * 100);
+			}
+		}
+		return 0;
+	}
+}
