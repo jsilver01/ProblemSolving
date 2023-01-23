22804f9d76408ac980226dd7e84954e6faeaea72
JUNGEUN <122519994+jsilver01@users.noreply.github.com>
2023-01-23 21:32:20 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+	int n;
+
+	scanf("%d", &n);
+	for (int i = 0; i <n; i++) {
+		for (int j = 1; j < n-i; j++) {
+			printf(" ");
+		}
+		for (int k = 0; k <= i; k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
