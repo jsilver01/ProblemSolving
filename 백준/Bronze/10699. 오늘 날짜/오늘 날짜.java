a5ea4ff770db02425d21a8bda5454d92949ccaaa
JUNGEUN <122519994+jsilver01@users.noreply.github.com>
2024-05-11 21:50:38 +0900

    [Bronze V] Title: 오늘 날짜, Time: 192 ms, Memory: 17092 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        System.out.println(today);
+    }
+}
