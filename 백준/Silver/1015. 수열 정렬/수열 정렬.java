ea6ef02683b735302dfe09e7bd09388093e957ab
JUNGEUN <122519994+jsilver01@users.noreply.github.com>
2024-10-07 17:04:03 +0900

    [Silver IV] Title: 수열 정렬, Time: 116 ms, Memory: 15884 KB -BaekjoonHub

--- 백준/Silver/1015. 수열 정렬/수열 정렬.java
+++ 백준/Silver/1015. 수열 정렬/수열 정렬.java
@@ -4,22 +4,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int[] a;
+	static boolean[] flag;
 	
 	public static void main(String[] args) throws IOException{
 		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		 int n = Integer.parseInt(br.readLine());
 		 
-		 int[] a = new int[n];
-		 boolean[] flag = new boolean[n];
+		 a = new int[n];
+		 flag = new boolean[n];
 		 StringTokenizer st = new StringTokenizer(br.readLine());
 		 for(int i = 0; i < n; i++) {
 			 a[i] = Integer.parseInt(st.nextToken());
 		 }
 		 
-		 int count = 0;
+		 checkArr(a);
+		 
+		 for(int i = 0; i < n; i++) {
+			 if(i==n-1) {
+				 System.out.print(a[i]);
+			 }else {
+				 System.out.print(a[i] + " ");
+			 }
+		 }
+	}
+	
+	public static void checkArr(int[] a) {
+		int count = 0;
 		 int targetnum = 1;
 		 for(int j = 0; j < 1001; j++) {
-			 for(int i = 0; i < n; i++) {
+			 for(int i = 0; i < a.length; i++) {
 				 if(targetnum==a[i]) {
 					 if(flag[i]!=true) { //한번도 건들여진적 없다는 의미
 						 a[i] = count;
@@ -31,15 +45,5 @@ public class Main {
 			 }
 			 targetnum++;
 		 }
-		 
-		 for(int i = 0; i < n; i++) {
-			 if(i==n-1) {
-				 System.out.print(a[i]);
-			 }else {
-				 System.out.print(a[i] + " ");
-			 }
-		 }
 	}
-	
-	
 }
