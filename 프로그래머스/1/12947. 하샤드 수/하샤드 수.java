366d08138f647b6a30786101531c71ef672ccd2a
JUNGEUN <122519994+jsilver01@users.noreply.github.com>
2026-05-11 22:31:22 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,24 +1,13 @@
 class Solution {
     public boolean solution(int x) {
         boolean answer = true;
+      
         int sum = 0;
-        int xLen = String.valueOf(x).length();
-        int realX = x;
-        for(int i = xLen; i > 0; i--){
-            if(i==1){
-                sum+=x;
-            }else{
-                sum+= x/Math.pow(10,i-1);
-                x = (int)(x % Math.pow(10,i-1));
-            }
+        String str = String.valueOf(x);
+        for(int i = 0; i < str.length(); i++){
+            sum += str.charAt(i) - '0';
         }
         
-        if(realX%sum==0){
-            answer = true;
-        }else{
-            answer = false;
-        }
-        
-        return answer;
+        return x%sum==0;
     }
 }
