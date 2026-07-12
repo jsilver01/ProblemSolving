d659a4f65a6b02faf9da12a0d6daba025a2733d9
JUNGEUN <122519994+jsilver01@users.noreply.github.com>
2026-07-12 14:35:42 +0900

    [level 2] Title: 프로세스, Time: 0.64 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        // 우선순위 큐
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int num : priorities){
+            pq.add(num);
+        }
+        
+        while(!pq.isEmpty()){
+            for(int i=0; i<priorities.length; i++) {
+				if(priorities[i] == pq.peek()) {
+					pq.poll();
+					answer++;
+					if(i == location)
+						return answer;
+				}    
+            }
+        }
+        
+        return answer;
+    }
+}
