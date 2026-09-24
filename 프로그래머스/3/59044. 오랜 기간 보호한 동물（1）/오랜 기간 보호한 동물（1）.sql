d86f4d1198fa343a14ecdac88a5ed040ce49d714
JUNGEUN <122519994+jsilver01@users.noreply.github.com>
2026-09-24 15:25:10 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,8 +1,6 @@
 -- 코드를 입력하세요
-select *
-from (
 SELECT a.name, a.datetime
 from animal_ins a
-where a.animal_id not in (select animal_id from animal_outs)
-order by a.datetime)
-where rownum <= 3;
+where not exists (select 1 from animal_outs b where b.animal_id = a.animal_id)
+order by a.datetime
+fetch first 3 row only;
